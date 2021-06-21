8a86fe60998ae694805d19bb885316c6552c19bd
Olanrewaju Sodeinde <lanre.sodeinde1@gmail.com>
2021-06-21 19:39:37 +0100

    Basic processing of information gathered from image

--- image-to-text.py
+++ image-to-text.py
@@ -1,13 +1,85 @@
 import pytesseract as tess
 from PIL import Image
 
-img = Image.open("images/test-image-2.jpeg")
+img = Image.open("images/test-image-1.jpeg")
 text = tess.image_to_string(img)
-
+text = text.strip()
+text = text.replace('\n', ' ')
+text = text.lower()
 print(text)
 
-# Process the text
-text = text.lower()
+print("\n")
 
+# Checks if it is for resident permit holders only
+# Means it is for permit holders at all times
+def for_resident_permit_holders_only(text):
+    check = text.find("resident permit holders only")
+    if(check >0):
+        print("For resident permit holders only: True")
+        return True
+    else:
+        print("For resident permit holders only: False")
+        return False
+
+# Checks if it is for resident permit holders only
+# Means it is for permit holders at all times
+def for_resident_permit_holders(text):
+    check = text.find("resident permit holders")
+    if(check >0):
+        print("For resident permit holders: True")
+        return True
+    else:
+        print("For resident permit holders: False")
+        return False
+
+
+# Checks if it is for resident permit holders only
+# Means it is for permit holders at all times
 def for_permit_holders_only(text):
-    print("true")
+    check = text.find("permit holders only")
+    if(check > 0):
+        print("For permit holders only: True")
+        return True
+    else:
+        print("For permit holders only: False")
+        return False
+
+def contains_no_return_within(text):
+    check = text.find("no return within")
+    if(check >= 0):
+        return check
+    else:
+        return False
+
+
+def get_no_return_time(text):
+    check = contains_no_return_within(text)
+    # Get no return time if it contains the return message
+    if(check >= 0):
+        print("Contains 'no return within' message: True")
+        check = check + 17
+        time = text[check: check+1]
+        time = time.strip()
+        units = text[check+1: check+6]
+        units = units.strip()
+        output = time +" " + units
+        print("No return within " + output)
+        return output
+
+
+        print("Timer started")
+
+    else:
+        print("Contains 'no return within' message: False")
+        print("No timer")
+
+
+
+
+
+for_resident_permit_holders_only(text)
+for_resident_permit_holders(text)
+for_permit_holders_only(text)
+get_no_return_time(text)
+
+print("\n")
