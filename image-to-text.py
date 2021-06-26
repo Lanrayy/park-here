c2168a356620cf416a418c6c2967803d19572d56
Olanrewaju Sodeinde <lanre.sodeinde1@gmail.com>
2021-06-26 17:11:10 +0100

    Updated Brief and monor changes to python file

--- image-to-text.py
+++ image-to-text.py
@@ -2,15 +2,30 @@ import pytesseract as tess
 from PIL import Image
 import time
 
-img = Image.open("images/test-image-3.jpeg")
 
-text = tess.image_to_string(img)
-text = text.strip()
-text = text.replace('\n', ' ')
-text = text.lower()
-print(text)
+
+# Main program
+def get_text_from_image(path):
+    img = Image.open(path)
+    text = tess.image_to_string(img)
+    text = text.strip()
+    text = text.replace('\n', ' ')
+    text = text.lower()
+    print(text)
+    return text
+
 
 print("\n")
+# Testing the function
+# path = "images/test-image-1.jpeg"
+# text = get_text_from_image(path)
+# assert text == "city of westminster pay by phone 02071259090 or text 07860022205 quoting location 8231 4 hours no return within 1 hour", "Comparison failed: Test image 1"
+
+path = "images/test-image-4.jpeg"
+text = get_text_from_image(path)
+
+print("\n")
+
 
 # Checks if it is for resident permit holders only
 # Means it is for permit holders at all times
@@ -46,6 +61,8 @@ def for_permit_holders_only(text):
         print("For permit holders only: False")
         return False
 
+# Check wehre the text contains no return within
+# return the index of the no return text
 def contains_no_return_within(text):
     check = text.find("no return within")
     if(check >= 0):
@@ -53,37 +70,50 @@ def contains_no_return_within(text):
     else:
         return False
 
-
+# Get the time if it contains the return message
 def get_no_return_time(text):
     check = contains_no_return_within(text)
-    # Get no return time if it contains the return message
+    # parse the input
     if(check >= 0):
+        # print(check)
         print("Contains 'no return within' message: True")
         check = check + 17
-        time = text[check: check+1]
+        time = text[check: check+2]
         time = time.strip()
-        units = text[check+1: check+6]
+        units = text[check+2: check+8]
         units = units.strip()
         output = time +" " + units
         print("No return within " + output)
         return output
-
-
-        print("Timer started")
-
     else:
         print("Contains 'no return within' message: False")
         print("No timer")
 
-seconds = time.time()
-time_struct = time.localtime(seconds)
-print(time_struct)
-current_time = time.asctime(time_struct)
-print(current_time)
+
+# Print time
+def get_current_time():
+    seconds = time.time()
+    time_struct = time.localtime(seconds)
+    print(time_struct)
+    current_time = time.asctime(time_struct)
+    print(current_time)
+
+
+def set_timer(minutes):
+    seconds = 60 * minutes
+    while(seconds >0):
+        print(seconds)
+        time.sleep(1)
+        seconds = seconds - 1
+        if(seconds == 15):
+            print("Alarm: Time to pick up your car!")
+    
+    
+        
 
 
 
-
+get_current_time()
 for_resident_permit_holders_only(text)
 for_resident_permit_holders(text)
 for_permit_holders_only(text)
